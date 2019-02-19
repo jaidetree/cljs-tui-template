6c580f4daa3ff2d277699e93abac9c6cc88c6188
Jay <jayzawrotny@gmail.com>
2019-02-19 17:05:45 -0500

    Refactored vertical-menu iteration

--- resources/leiningen/new/cljs_cli/src/views/menus.cljs
+++ resources/leiningen/new/cljs_cli/src/views/menus.cljs
@@ -9,29 +9,51 @@
            (keys)
            (map-indexed vector)
            (filter (fn [[idx v]] (= v target)))
-           (keys)
+           (first)
            (first)))
 
-
-(defn next
+(defn next-option
   [current options]
-  (let [current-idx (find-index current options)]
-   (min (inc current-idx) (dec (count options)))))
+  (let [total (count options)
+        current-idx (find-index current options)
+        next-idx (inc current-idx)]
+    (-> options
+        (vec)
+        (nth (if (< next-idx total) next-idx 0))
+        (key))))
 
-(defn prev
+(defn prev-option
   [current options]
-  (let [current-idx (find-index current options)]
-    (max (dec current-idx) 0)))
+  (let [total (count options)
+        current-idx (find-index current options)
+        prev-idx (dec current-idx)]
+    (-> options
+        (vec)
+        (nth (if (< prev-idx 0) (dec total) prev-idx))
+        (key))))
 
 (defn vertical-menu
   [{:keys [options default on-select]}]
-  (let [selected (r/atom (or default (->> options first key)))]
-    (with-keys @screen {["j"] #(swap! selected next options)
-                        ["k"] #(swap! selected prev options)}
-      [:box#menu
-       {}
-       (for [[value label] options]
-         [:text {:content label
-                 :key value
-                 :style {:bg (when (= value selected) :green)
-                         :fg (when (= value selected) :white)}}])])))
+  (r/with-let [selected (r/atom (or default (->> options first key)))]
+    (with-keys @screen {["j"] #(swap! selected next-option options)
+                        ["k"] #(swap! selected prev-option options)
+                        ["enter"] #(on-select @selected)}
+      (let [current @selected]
+        [:box#menu
+         {:height (count options)}
+         (for [[idx [value label]] (map-indexed vector options)]
+           [:box {:key value
+                  :top idx
+                  :style {:bg (when (= value current) :green)
+                          :fg (when (= value current) :white)}
+                  :content label}])]))))
+
+(comment
+ (def options {:about "About"
+               :help "Help"
+               :library "Library"})
+ (def sel (atom :about))
+ (println @sel)
+ (println (swap! sel next-option options))
+ (reset! sel :about)
+ (find-index :help options))
