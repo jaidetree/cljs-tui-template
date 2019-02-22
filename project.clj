3eec984ea18e40350f17a16e3c5b41eac86da8ca
Jay <jayzawrotny@gmail.com>
2019-02-22 17:49:52 -0500

    Switched license to ASL2 to give devs a bit more freedom

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject cljs-tui/lein-template "0.1.0-SNAPSHOT"
   :description "A lein template for creating very rich ClojureScript Terminal-User-Interface (TUI) applications powered by reagent + blessed views"
   :url "https://github.com/eccentric-j/cljs-tui-template"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "The Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :eval-in-leiningen true
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}})
